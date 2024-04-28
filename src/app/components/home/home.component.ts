f110942d64ed9ea08aff7accc8f04cb387e8d1b0
Markson Arguello <marksonarguello@gmail.com>
2024-04-28 19:59:04 -0300

    Implementação da busca por código da ordem de serviço

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,25 +2,46 @@ import { Component } from '@angular/core';
 import { ServiceOrderService } from '../../services/service-order.service';
 import { ServiceOrder } from '../../model/service-order';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  formulario!: FormGroup;
   seviceOrderList: ServiceOrder[] = [];
   page: number = 1;
+  loading = false;
 
-  constructor(private seviceOrderService: ServiceOrderService) {}
+  constructor(
+    private seviceOrderService: ServiceOrderService,
+    private formBuilder: FormBuilder,
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.formulario = this.formBuilder.group({
+      serviceOrderCode: [''],
+    });
+  }
 
-  findByServiceOrderCode(serviceOrderCode: string, page: number) {
+  findByServiceOrderCode() {
+    this.page = 1;
+    this.loading = true;
+    this.findByServiceOrderCodeAndPage(this.f().serviceOrderCode, this.page);
+  }
+
+  private findByServiceOrderCodeAndPage(serviceOrderCode: string, page: number) {
     this.seviceOrderService.findByServiceOrderCode(serviceOrderCode, page).subscribe((PaginatedResponse) => {
       this.seviceOrderList = PaginatedResponse.objects;
+      this.loading = false;
     });
+  }
+
+  private f() {
+    return this.formulario.value;
   }
 }
